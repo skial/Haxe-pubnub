c67f0dfdc93ef3b3b99afdeccb2fe357834cad61
Skial Bainn <skial.bainn@gmail.com>
2010-09-07 17:22:02 +0100

    Added the last of the javascript methods. Not yet tested but dont see why it wouldnt work.

--- com/pubnub/js/PUBNUB.hx
+++ com/pubnub/js/PUBNUB.hx
@@ -15,15 +15,40 @@ extern class PUBNUB {
 	/**
 	 * 
 	 * @param	options	-	takes an anonymous object eg { channel:my_channel }
-	 * @param	?handler
+	 * @param	?handler	-	callback method
 	 */
 	public static function subscribe(options:Dynamic, ?handler:Dynamic):Void;
 	
 	/**
 	 * 
-	 * @param	options	-	takes an anonymous object eg {channel:my_channel, message:your_message }
-	 * @param	?handler
+	 * @param	options	-	takes an anonymous object eg { channel:my_channel, message:your_message }
+	 * @param	?handler	-	callback method
 	 */
 	public static function publish(options:Dynamic, ?handler:Dynamic):Void;
+	
+	/**
+	 * 
+	 * @param	options	-	takes an anonymous object eg { channel:my_channel }
+	 */
+	public static function unsubscribe(options:Dynamic):Void;
+	
+	/**
+	 * 
+	 * @param	options	-	takes an anonymous object eg { channel:my_channel, limit:10 }
+	 * @param	?handler	-	callback method
+	 */
+	public static function history(options:Dynamic, ?handler:Dynamic):Void;
+	
+	/**
+	 * 
+	 * @param	handler	-	callback method
+	 */
+	public static function uuid(handler:Dynamic):Void;
+	
+	/**
+	 * 
+	 * @param	handler	-	callback method
+	 */
+	public static function time(handler:Dynamic):Void;
 	
 }
